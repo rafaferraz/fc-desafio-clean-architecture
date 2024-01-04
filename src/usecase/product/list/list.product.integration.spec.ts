391efdaca8a89b2a38c613ff0d48144360f4bf2a
Rafaela Cristina Ferraz <69472079+rafaferraz@users.noreply.github.com>
2024-01-04 10:56:53 -0300

    Uploading Use Cases Challenge

---
+++ src/usecase/product/list/list.product.integration.spec.ts
@@ -0,0 +1,47 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import ListProductUseCase from "./list.product.usecase";
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import Product from "../../../domain/product/entity/product";
+
+describe("Test list products use case", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true },
+        });
+
+        await sequelize.addModels([ProductModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("should list products", async () => {
+        const productRepository = new ProductRepository();
+        const useCase = new ListProductUseCase(productRepository);
+
+        const product1 = new Product("123", "Product 1", 10);
+        await productRepository.create(product1);
+
+        const product2 = new Product("456", "Product 2", 20);
+        await productRepository.create(product2);
+
+        const output = await useCase.execute({});
+
+        expect(output.products.length).toBe(2);
+        expect(output.products[0].id).toBe(product1.id);
+        expect(output.products[0].name).toBe(product1.name);
+        expect(output.products[0].price).toBe(product1.price);
+        expect(output.products[1].id).toBe(product2.id);
+        expect(output.products[1].name).toBe(product2.name);
+        expect(output.products[1].price).toBe(product2.price);
+    });
+});
